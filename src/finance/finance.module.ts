0c6cd17eff9f0cdb4eef897609ccc92373c43d55
sahefi <alvianto_putra_29rpl@student.smktelkom-mlg.sch.id>
2024-02-12 10:56:02 +0700

    bismillah

--- src/finance/finance.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FinanceService } from './finance.service';
-import { FinanceController } from './finance.controller';
-
-@Module({
-  controllers: [FinanceController],
-  providers: [FinanceService],
-})
-export class FinanceModule {}
--- src/gco/dto/update-gco.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateGcoDto } from './create-gco.dto';
-
-export class UpdateGcoDto extends PartialType(CreateGcoDto) {}
